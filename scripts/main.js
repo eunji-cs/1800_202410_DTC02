146d2fe1a47870ddbc70a7542189a827cc48cc47
Eunji Kwon <kwonge08e@gmail.com>
2024-03-18 20:14:12 -0700

    Fix logout button to navigate the login page

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,20 @@
 // loadSkeleton();
 
 function insertNameFromFirestore() {
-  // Check if the user is logged in:
+
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-      currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+      console.log(user.uid);
+      currentUser = db.collection("users").doc(user.uid);
       currentUser.get().then(userDoc => {
-        // Get the user name
+
         let userName = userDoc.data().name;
         console.log(userName);
-        //$("#name-goes-here").text(userName); // jQuery
+
         document.getElementById("name-goes-here").innerText = userName;
       })
     } else {
-      console.log("No user is logged in."); // Log a message when no user is logged in
+      console.log("No user is logged in.");
     }
   })
 }
@@ -27,21 +27,14 @@ insertNameFromFirestore();
 
 function getNameFromAuth() {
   firebase.auth().onAuthStateChanged(user => {
-    // Check if a user is signed in:
+
     if (user) {
-      // Do something for the currently logged-in user here: 
-      console.log(user.uid); //print the uid in the browser console
-      console.log(user.displayName);  //print the user name in the browser console
+
+      console.log(user.uid);
+      console.log(user.displayName);
       userName = user.displayName;
 
-      //method #1:  insert with JS
-      //document.getElementById("name-goes-here").innerText = userName;    
-
-      //method #2:  insert using jquery
-      $("#name-goes-here").text(userName); //using jquery
-
-      //method #3:  insert using querySelector
-      //document.querySelector("#name-goes-here").innerText = userName
+      $("#name-goes-here").text(userName);
 
     } else {
       // No user is signed in.
