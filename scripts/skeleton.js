fde80c6d7704e5f26ddb5ff35b0dc31c78204a98
Eunji Kwon <kwonge08e@gmail.com>
2024-03-27 15:02:40 -0700

    Update skeleton.js

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -14,3 +14,15 @@ function loadSkeleton() {
 }
 
 loadSkeleton(); //invoke the function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
+}
