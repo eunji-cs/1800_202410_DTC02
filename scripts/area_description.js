fa37210f57531ca476557bb0499c473ffb768e27
dlee537 <dlee537@my.bcit.ca>
2024-03-27 20:53:42 -0700

    Link with neighbourhood ID

--- scripts/area_description.js
+++ scripts/area_description.js
@@ -1,21 +1,19 @@
 function displayNeighbourhoodInfo() {
   let params = new URL(window.location.href); //get the url of search bar
   let ID = params.searchParams.get("docID"); //get value for key "id"
-  console.log("id", ID);
+  console.log("id:", ID);
 
   db.collection("neighbourhood_info")
     .doc(ID)
     .get()
     .then(doc => {
-      thisNeighbourhood = doc.data();
-      neighbourhoodCode = thisNeighbourhood.code;
-      neighbourhoodName = doc.data().title;
+      var thisNeighbourhood = doc.data();
+      var neighbourhoodCode = thisNeighbourhood.code;
+      var neighbourhoodName = doc.data().title;
 
-      console.log(thisNeighbourhood);
-      console.log(thisNeighbourhood.description);
-
-      console.log
-      console.log("description", document.getElementById("description"))
+      // console.log(thisNeighbourhood);
+      // console.log(thisNeighbourhood.description);
+      // console.log("description", document.getElementById("description"))
 
       document.getElementById("description").innerHTML = thisNeighbourhood.description;
       document.getElementById("title").innerHTML = thisNeighbourhood.title;
@@ -28,6 +26,8 @@ function displayNeighbourhoodInfo() {
       document.getElementById("safety_score").innerHTML = thisNeighbourhood.biker_score;
       document.getElementById("park_score").innerHTML = thisNeighbourhood.biker_score;
 
+
+      document.getElementById("reviewButton").href = "reviews.html?docID="+ID; // This grabs the URL ID from this page and allows access to the specific neighbourhood's review page.
     });
 }
 displayNeighbourhoodInfo()
--- scripts/list.js
+++ scripts/list.js
@@ -1,30 +1,31 @@
 function displayListItemsDynamically(collection) {
-    let itemTemplate = document.getElementById("listItemTemplate");
+    let itemTemplate = document.getElementById("listItemTemplate"); // Set itemTempalte as HTML template.
 
+    // Get Firebase collection.
     db.collection(collection).get()
         .then(allItems=> {
-            console.log('Loaded!')
-
             allItems.forEach(doc => {
-                // get database info
+                // Get database information.
                 var name = doc.data().title;
-
-                console.log(doc.data().title)
-
+                var docID = doc.id;
+                
                 let newItem = itemTemplate.content.cloneNode(true); // Clone html.
 
                 newItem.querySelector('#neighbourhood-name-goes-here').innerHTML = name;
+                newItem.querySelector('a').href = "area_description.html?docID="+docID;
 
-
-                document.getElementById("cards-go-here").appendChild(newItem);
+                document.getElementById("cards-go-here").appendChild(newItem); // Add the template to the HTML.
             })
-
-
         })
-
-
-
-
 }
 
 displayListItemsDynamically("neighbourhood_info");
+
+
+// Saves the neighbourhood ID into local storage. 
+function saveNeighbourhoodIDAndRedirect(){
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('docID', ID);
+    window.location.href = 'area_description.html';
+}
